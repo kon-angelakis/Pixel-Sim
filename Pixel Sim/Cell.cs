4bca211069be8a7ff47628dc3b96ffa9742858c4
p21001 <p21001@unipi.gr>
2024-03-05 02:27:02 +0200

    Added files

---
+++ Pixel Sim/Cell.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixel_Sim
+{
+    internal class Cell
+    {
+
+        private int x;
+        private int y;
+
+        private int size;
+
+        private int value = 0;
+
+        private Texture2D texture;
+
+        private Color color;
+
+        public Cell down, left, right, down_left, down_right;
+
+        public Cell(int x, int y, int size, Texture2D texture, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.texture = texture;
+            this.color = color;
+            this.size = size;
+        }
+
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(texture, new Rectangle(x, y, size, size), color);
+        }
+
+        public void UpdateNeighbours(Cell left, Cell right, Cell down, Cell down_left, Cell down_right)
+        {
+            this.left = left;
+            this.right = right;
+            this.down = down;
+            this.down_left = down_left;
+            this.down_right = down_right;
+        }
+
+
+        public void UpdateCell()
+        {
+            if (this.value != 0)
+            {
+                if (this.down.getValue() == 0)
+                {
+                    this.setValue(0);
+                    this.down.setValue(1);
+
+                }
+                else if (this.down_right.getValue() == 0)
+                {
+                    this.setValue(0);
+                    this.down_right.setValue(1);
+                }
+                else if (this.down_left.getValue() == 0)
+                {
+                    this.setValue(0);
+                    this.down_left.setValue(1);
+                }
+
+            }
+        }
+
+        public void setValue(int n)
+        {
+            this.value = n;
+        }
+
+        public int getValue()
+        {
+            return (this.value);
+        }
+
+    }
+}
--- Pixel Sim/Game1.cs
+++ Pixel Sim/Game1.cs
@@ -1,52 +1,146 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
 
 namespace Pixel_Sim
 {
     public class Game1 : Game
     {
+        private const int resX = 1920;
+        private const int resY = 1080;
+
+        private MouseState currentMouse, prevMouse;
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private Microsoft.Xna.Framework.Graphics.SpriteBatch _spriteBatch;
+        private Texture2D texture;
+
+        private Random r;
+
+        //Make them so they are multiples of resX and resY standard 16:9 resolution
+        private int rows = 9 * 20;
+        private int cols = 16 * 20;
+        private int cell_size;
+
+        private Cell[,] grid, grid_drawn;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = resX;
+            _graphics.PreferredBackBufferHeight = resY;
+            _graphics.IsFullScreen = true;
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
+
+            TargetElapsedTime = TimeSpan.FromSeconds(1.0f / 60.0f);
         }
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new Microsoft.Xna.Framework.Graphics.SpriteBatch(GraphicsDevice);
+            texture = new Texture2D(GraphicsDevice, 1, 1);
+            texture.SetData(new[] { Color.White });
+            r = new Random();
 
-            // TODO: use this.Content to load your game content here
+
+            grid = new Cell[cols, rows];
+            grid_drawn = new Cell[cols, rows];
+
+            //dynamic cell size according to set resolution
+            cell_size = resX / cols;
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    grid[x, y] = new Cell(x * resY / rows, y * resX / cols, cell_size, texture, Color.Gold);
+                    grid_drawn[x, y] = new Cell(x * resY / rows, y * resX / cols, cell_size, texture, Color.Gold);
+
+                }
+            }
+
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            CheckControls();
 
-            // TODO: Add your update logic here
+            for (int x = cols - 2; x >= 1; x--)
+            {
+                for (int y = rows - 2; y >= 0; y--)
+                {
+                    //Don't update empty cells save resources 
+                    if (grid_drawn[x, y].getValue() != 0)
+                    {
+                        grid_drawn[x, y].UpdateNeighbours(grid_drawn[x - 1, y], grid_drawn[x + 1, y], grid_drawn[x, y + 1], grid_drawn[x - 1, y + 1], grid_drawn[x + 1, y + 1]);
+                        grid_drawn[x, y].UpdateCell();
 
+                    }
+
+                }
+            }
             base.Update(gameTime);
+
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(new Color(0, 0, 0));
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            //Placement preview
+            grid_drawn[(int)Math.Floor((double)Mouse.GetState().X / cell_size), (int)Math.Floor((double)Mouse.GetState().Y / cell_size)].Draw(_spriteBatch);
+
+            foreach (Cell c in grid_drawn)
+            {
+                //If not empty cell draw
+                if (c.getValue() != 0)
+                    c.Draw(_spriteBatch);
+
+            }
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
+
+        private void CheckControls()
+        {
+            double mouseX = Mouse.GetState().X;
+            double mouseY = Mouse.GetState().Y;
+
+            double mouseCellX = Math.Floor(mouseX / cell_size);
+            double mouseCellY = Math.Floor(mouseY / cell_size);
+
+            prevMouse = currentMouse;
+            currentMouse = Mouse.GetState();
+
+            if (Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
+                Exit();
+
+
+            if (currentMouse.LeftButton == ButtonState.Pressed)
+            {
+                if (mouseCellX > 0 && mouseCellX < cols - 1)
+                    grid_drawn[(int)mouseCellX, (int)mouseCellY].setValue(1);
+
+            }
+
+            //Clear canvas
+            if (currentMouse.MiddleButton == ButtonState.Pressed)
+            {
+                foreach (Cell c in grid)
+                {
+                    c.setValue(0);
+                }
+            }
+
+
+        }
+
     }
 }
